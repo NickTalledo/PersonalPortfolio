9c617f275bb167fd7aec71db2df0fa07a8552e99
Nick Talledo <nicolastalledo13@gmail.com>
2023-07-24 01:05:11 -0400

    favicon2

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <style id={inter.styleId}>{inter.fontStylesheet}</style>
+        <style id={inter.style}>{inter.fontStylesheet}</style>
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
