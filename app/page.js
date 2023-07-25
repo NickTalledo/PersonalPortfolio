8d281fa4f2c54f1f48054ed884601525d076db14
Nick Talledo <nicolastalledo13@gmail.com>
2023-07-25 10:32:37 -0400

    knowledge

--- app/page.js
+++ app/page.js
@@ -76,7 +76,7 @@ export default function Home() {
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
               Computer Science Student interested in building web-applications.
-              Knowledable in both Frontend and Backend!
+              Knowledgeable in both Frontend and Backend!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a
